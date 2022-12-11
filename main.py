29d2967cc905430a7dbd67d7cf80a325eb287add
Carim Shrif <carimshrif@gmail.Com>
2022-12-11 22:10:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -31,6 +31,7 @@ X = data.iloc[:, :-1]
 y = data.iloc[:, -1]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 under_sampler = RandomUnderSampler(random_state=42)
+#balancing it
 X_train, y_train = under_sampler.fit_resample(X_train, y_train)
 print(' Training data =', len(X_train), '\n', 'Testing data =', len(X_test), '\n', 'Total data length = ',
       len(X_train) + len(X_test))
